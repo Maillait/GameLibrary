32a077bbd457be6cf8a2252c8774913451f546b4
Maillait <williamottenbreit@gmail.com>
2025-09-22 17:23:30 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
 // event listeners, and game code goes here, won't be much until all other modules are complete, though.
+import (drawStats, clearHUD, drawCraft, drawHotbar) from "./drawHud.js";
+import (drawTiles, drawEntities, drawPlayer, drawLightingEffects, drawForegro0und, drawBackground, clearDraw) from "./drawMain.js";
+import (checkColliders, completeCollisions, addPlayerVelocityVectors, pathfind, enemyCollisions) from "./physics.js";
+import (drawBox, clearBox, timeDraw, hideBox, showBox) from "./textBox.js";
+import (drawTitle, timedTitle, hideTitle, clearTitle) from "./title.js";
