b61c535c1d063156e996d7b7866ebd2a8b36a6a9
Maillait <williamottenbreit@gmail.com>
2025-09-22 16:51:26 -0600

    Create textBox.js

---
+++ textBox.js
@@ -0,0 +1,14 @@
+// will draw box with text
+export function drawBox(var text) {};
+
+// will hide/clear box
+export function clearBox() {};
+
+// will draw with a slight delay after each character
+export function timeDraw(var text, var timer) {};
+
+// will hide box
+export function hideBox() {};
+
+// will show box
+export function showBox() {};
