aa472509834dfe914f49b1eaebe7cd5ddb9fecc9
Maillait <williamottenbreit@gmail.com>
2025-09-22 17:04:11 -0600

    Create drawMain.js

---
+++ drawMain.js
@@ -0,0 +1,13 @@
+export function drawTiles() {};
+
+export function drawEntities() {};
+
+export function drawPlayer() {};
+
+export function drawLightingEffects() {};
+
+export function drawForegro0und() {};
+
+export function drawBackground() {};
+
+export function clearDraw() {};
