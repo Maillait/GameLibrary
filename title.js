24360f1dc7f49993b37c0d9c2958d3fe938d09c4
Maillait <williamottenbreit@gmail.com>
2025-09-22 16:55:14 -0600

    Create title.js

---
+++ title.js
@@ -0,0 +1,7 @@
+export function drawTitle(var title, var subtext) {};
+
+export function timedTitle(var title, var subtext, var delay) {};
+
+export function hideTitle() {};
+
+export function clearTitle() {};
