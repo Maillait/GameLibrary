e6249da116f3ca5e86886ffe4a750c7c17d9aa46
Maillait-school <WO834575@isidore.redeemer.ab.ca>
2025-09-23 13:10:16 -0600

    Update main.mjs

--- main.mjs
+++ main.mjs
@@ -1,6 +1,6 @@
 // event listeners, and game code goes here, won't be much until all other modules are complete, though.
-import (drawStats, clearHUD, drawCraft, drawHotbar) from "./drawHud.mjs";
-import (drawTiles, drawEntities, drawPlayer, drawLightingEffects, drawForegro0und, drawBackground, clearDraw) from "./drawMain.mjs";
-import (checkColliders, completeCollisions, addPlayerVelocityVectors, pathfind, enemyCollisions) from "./physics.mjs";
-import (drawBox, clearBox, timeDraw, hideBox, showBox) from "./textBox.mjs";
-import (drawTitle, timedTitle, hideTitle, clearTitle) from "./title.mjs";
+import {drawStats, clearHUD, drawCraft, drawHotbar} from "./drawHud.mjs";
+import {drawTiles, drawEntities, drawPlayer, drawLightingEffects, drawForegro0und, drawBackground, clearDraw} from "./drawMain.mjs";
+import {checkColliders, completeCollisions, addPlayerVelocityVectors, pathfind, enemyCollisions} from "./physics.mjs";
+import {drawBox, clearBox, timeDraw, hideBox, showBox} from "./textBox.mjs";
+import {drawTitle, timedTitle, hideTitle, clearTitle} from "./title.mjs";
