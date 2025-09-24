d34aa1e82c7ec010a723233c9d31b9cfe83b04ab
Maillait-school <WO834575@isidore.redeemer.ab.ca>
2025-09-24 12:04:30 -0600

    Update title.mjs

--- title.mjs
+++ title.mjs
@@ -1,7 +1,29 @@
-export function drawTitle( title, subtext) {};
+export function drawTitle(title, subtext) {
+  document.getElementById("title").style.visibility = "visible";
+  document.getElementById("title").innerHTML = "<h1>"+title+"<h1><p>"+subtext+"<p>";
+  return;
+};
 
-export function timedTitle(title, subtext, delay) {};
+export function timedTitle(title, subtext, delay) {
+  document.getElementById("title").style.visibility = "visible";
+  document.getElementById("title").innerHTML = "<h1>"+title+"<h1><p>"+subtext+"<p>";
+  function waitFunc() {
+    if (waitcheck) clearTitle();
+    else waitcheck++;
+    return;
+  } 
+  let waitCheck = 0;
+  let waitTime = setInterval(waitFunc, delay);
+  return;
+};
 
-export function hideTitle() {};
+export function hideTitle() {
+  document.getElementById("title").style.visibility = "hidden";
+  return;
+};
 
-export function clearTitle() {};
+export function clearTitle() {
+  document.getElementById("title").innerText = "";
+  document.getElementById("title").style.visibility = "hidden";
+  return;
+};
