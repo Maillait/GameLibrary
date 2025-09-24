55a56001441fd12580e8fa5b9462557323f5aac9
Maillait-school <WO834575@isidore.redeemer.ab.ca>
2025-09-24 13:03:51 -0600

    Update main.mjs

--- main.mjs
+++ main.mjs
@@ -3,6 +3,7 @@ import {drawStats, clearHUD, drawCraft, drawHotbar} from "./drawHud.mjs";
 import {drawTiles, drawEntities, drawPlayer, drawLightingEffects, drawForegro0und, drawBackground, clearDraw} from "./drawMain.mjs";
 import {checkColliders, completeCollisions, addPlayerVelocityVectors, pathfind, enemyCollisions} from "./physics.mjs";
 import {drawBox, clearBox, timeDraw, hideBox, showBox} from "./textBox.mjs"; // complete
-import {drawTitle, timedTitle, hideTitle, clearTitle} from "./title.mjs";
+import {drawTitle, timedTitle, hideTitle, clearTitle} from "./title.mjs"; // complete
 
-timedTitle("---World 1---", "The Forest", 4000);
+timedTitle("---World 1---", "The Forest", 500);
+timeDraw("Welcome to The Forest, and prepare to be destroyed by the many monsters and oddly kind travelors who just want to test line wrapping.", 30)
