8f7256c820b44c92cf544fdd0abe96a1561d0d85
Maillait-school <WO834575@isidore.redeemer.ab.ca>
2025-09-24 11:56:24 -0600

    Update main.mjs

--- main.mjs
+++ main.mjs
@@ -2,5 +2,5 @@
 import {drawStats, clearHUD, drawCraft, drawHotbar} from "./drawHud.mjs";
 import {drawTiles, drawEntities, drawPlayer, drawLightingEffects, drawForegro0und, drawBackground, clearDraw} from "./drawMain.mjs";
 import {checkColliders, completeCollisions, addPlayerVelocityVectors, pathfind, enemyCollisions} from "./physics.mjs";
-import {drawBox, clearBox, timeDraw, hideBox, showBox} from "./textBox.mjs";
+import {drawBox, clearBox, timeDraw, hideBox, showBox} from "./textBox.mjs"; // complete
 import {drawTitle, timedTitle, hideTitle, clearTitle} from "./title.mjs";
