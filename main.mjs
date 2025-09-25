bbd9728cddc2f63a6aa3865a8a6aabcccd67d2bb
Maillait-school <WO834575@isidore.redeemer.ab.ca>
2025-09-25 13:12:42 -0600

    Update main.mjs

--- main.mjs
+++ main.mjs
@@ -9,12 +9,10 @@ import {currentLevel, nonPlayerCharacters, enemies, levelForeground, levelBackgr
 // testing functions
 defineLevel(1);
 drawTiles();
-drawLightingEffects(20);
 timedTitle("---World 1---", "The Forest", 1000);
 timeDraw("Welcome to The Forest, and prepare to be destroyed by the many monsters and oddly kind travelors who just want to test line wrapping.", 30);
 
 document.addEventListener("mousemove", (event) => {
   drawTitle("---World 2---", "Tests");
   clearBox();
-  timeDraw("Isn't this just painfully slow, you've got to love that the function has a speed control. What did you call me? how dare you, you're more of a sloth tghan this text will ever be!!!...@@@###$$$%%^^^&&&***((())) You actually stayed?? Thank you (;", 100);
 });
